9bab33abf83bc4cd69fba50759c48a21464877f4
zhezhe-Arthur <46439838+zhezhe-Arthur@users.noreply.github.com>
2020-03-10 20:22:46 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <index></index>
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+import index from './components/index'
+export default {
+  name: 'App',
+  components: {
+    index
+  },
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+@import './assets/css/base.css';
+
+</style>
